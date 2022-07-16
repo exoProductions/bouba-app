0ad9e148483f309e7bdb14518c2d2fffb6c65e5a
Dominic <dominic.gut@gmx.ch>
2022-07-16 14:09:41 +0200

    home swipeview style and functionality

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+import { delay } from 'rxjs/operators';
 import { Mentee } from 'src/app/models/mentee';
 import { Peer } from 'src/app/models/peer';
 import { InitService } from 'src/app/services/init.service';
@@ -19,21 +21,29 @@ export class HomePage implements OnInit {
     scrollbar: { draggable: true },
   };
 
-  constructor(private navigationService:NavigationService,private initService:InitService,private peerMenteeService:PeerMenteeService) { }
+  chatIcon = faComments;
+
+  constructor(private navigationService: NavigationService, private initService: InitService, private peerMenteeService: PeerMenteeService) { }
 
   ngOnInit() {
     Swiper.use([Pagination]);
-    this.navigationService.currentPageInd=1;
+    this.navigationService.currentPageInd = 1;
   }
 
-  getPeerList():Peer[]{
+  getPeerList(): Peer[] {
     return this.peerMenteeService.peerList;
   }
-  getMenteeList():Mentee[]{
+  getMenteeList(): Mentee[] {
     return this.peerMenteeService.menteeList;
   }
-  getIsMentee():boolean{
+  getIsMentee(): boolean {
     return this.initService.userdata.isMentee;
+  }
+  getProfilePicture(ind: number): string {
+    if (this.getIsMentee()) {
+      return this.peerMenteeService.peerList[ind].firstNickname + ".jpg";
+    }
+
   }
 
 }
